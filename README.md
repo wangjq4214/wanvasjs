d2b3625ed8d6493f267324d1f62570cb7c3e625d
Jinquan Wang <w1637894214@163.com>
2022-06-04 16:28:34 +0800

    🎉 feat: 初始化仓库

---
+++ README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: wangjq4214
+ * @Date: 2022-06-04 16:24:06
+ * @LastEditTime: 2022-06-04 16:25:11
+ * @LastEditors: wangjq4214
+-->
+# wanvasjs
+
+Draws in the Canvas panel using the DOM form.
---
+++ package/core/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: wangjq4214
+ * @Date: 2022-06-04 16:18:36
+ * @LastEditTime: 2022-06-04 16:18:46
+ * @LastEditors: wangjq4214
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
